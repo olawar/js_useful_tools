ef13b31691c3e19bcee2f8af4692bbe51e43676f
olawar <warzecha.ola@gmail.com>
2015-10-16 11:09:41 +0200

    Created animation effect to sticky menu

--- stickyMenu/js/app.js
+++ stickyMenu/js/app.js
@@ -8,14 +8,21 @@ $(function(){
 
     var menu = $("#mainMenu");
     var lastPositionTop = 0;
+    var menuHeight = menu.height();
 
     $(window).on("scroll", function(){
         if(menu.hasClass("sticky") === false && $(this).scrollTop() > menu.offset().top){
             lastPositionTop = menu.offset().top;
             menu.addClass("sticky");
+            menu.animate({
+                height: (menuHeight/4).toString() + "px"
+            }, 500);
         }
         if(menu.hasClass("sticky") && $(this).scrollTop() < lastPositionTop){
             menu.removeClass("sticky");
+            menu.animate({
+                height: (menuHeight).toString() + "px"
+            }, 500);
         }
     });
 
