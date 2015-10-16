fe3e396d22734fc313ab176fbcaa8fc9df2d5f20
olawar <warzecha.ola@gmail.com>
2015-10-16 12:01:08 +0200

    Created simple scroll spy for sticky menu

--- stickyMenu/js/app.js
+++ stickyMenu/js/app.js
@@ -9,6 +9,8 @@ $(function(){
     var menu = $("#mainMenu");
     var lastPositionTop = 0;
     var menuHeight = menu.height();
+    var paragraphs = $("p");
+    var links = $(".nav a");
 
     $(window).on("scroll", function(){
         if(menu.hasClass("sticky") === false && $(this).scrollTop() > menu.offset().top){
@@ -24,7 +26,30 @@ $(function(){
                 height: (menuHeight).toString() + "px"
             }, 500);
         }
+
+        paragraphs.each(function(index){
+            if(index + 1 >= paragraphs.length){
+                if( paragraphs.eq(index).offset().top < $(window).scrollTop()){
+                    links.eq(index).addClass("active");
+                }
+                else {
+                    links.eq(index).removeClass("active");
+                }
+            }
+            else {
+                if( paragraphs.eq(index).offset().top < $(window).scrollTop() &&  paragraphs.eq(index + 1).offset().top > $(window).scrollTop()){
+                    links.eq(index).addClass("active");
+                }
+                else {
+                    links.eq(index).removeClass("active");
+                }
+            }
+
+        });
+
     });
+
+
 
 
 });
