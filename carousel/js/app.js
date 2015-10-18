b75056e6858e60f9e07a73b84ba0a8e475c41b8f
olawar <warzecha.ola@gmail.com>
2015-10-18 17:38:53 +0200

    Modified carousel mini project - removed unnecessary code lines

--- carousel/js/app.js
+++ carousel/js/app.js
@@ -6,43 +6,18 @@ $(function(){
     var images = $(".slider li");
     var visibleImage = 0;
 
-    //images.eq(visibleImage).show();
-    //
-    //$(".right").click(function(){
-    //    images.eq(visibleImage).hide();
-    //    visibleImage++;
-    //    if(visibleImage >= images.length){
-    //        visibleImage=0;
-    //    }
-    //    images.eq(visibleImage).show();
-    //});
-    //
-    //$(".left").click(function(){
-    //    images.eq(visibleImage).hide();
-    //    visibleImage--;
-    //    if(visibleImage < 0){
-    //        visibleImage=images.length-1;
-    //    }
-    //    images.eq(visibleImage).show();
-    //});
+    images.eq(visibleImage).show();
 
-
-    images.click(function(event){
-        images.eq(visibleImage).hide(0);
-        if(event.offsetX < $(this).width()/2){
-            visibleImage--;
-        }
-        else{
-            visibleImage++;
-        }
-        if(visibleImage < 0){
-            visibleImage=images.length-1;
-        }
+    $(".right").click(function(){
+        images.eq(visibleImage).hide();
+        visibleImage++;
         if(visibleImage >= images.length){
             visibleImage=0;
         }
         images.eq(visibleImage).show();
+    });
 
+    $(".left").click(function(){
         images.eq(visibleImage).hide();
         visibleImage--;
         if(visibleImage < 0){
@@ -50,6 +25,7 @@ $(function(){
         }
         images.eq(visibleImage).show();
     });
+
 
 
 
